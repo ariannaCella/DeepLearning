21fb8ab5a9205016f9cf895c386971bf41cc22d4
Arianna Cella <117760885+ariannaCella@users.noreply.github.com>
2025-05-05 17:14:09 +0200

    Update model.py

--- model.py
+++ model.py
@@ -8,22 +8,35 @@ def get_model(model_name, num_classes=196):
     if model_name == "resnet18":
         model = torchvision.models.resnet18(weights='IMAGENET1K_V1')
         
-        # Funzione per congelare i layer
+        '''
+        Congelo tutti i layers -> Cong1234
+
+        # funzione per congelare i layers del modello
+        def set_parameter_requires_grad(model, req_grad = False):
+            for param in model.parameters():
+                param.requires_grad = req_grad
+        set_parameter_requires_grad(model, req_grad=False)
+        '''
+
+
+        '''
+        Congelo primo e secondo layers -> Cong12
+        
+        # congela tutti i layer tranne layer3 e layer4
         def set_parameter_requires_grad(net, req_grad=False):
             for name, param in net.named_parameters():
                 if "layer3" not in name and "layer4" not in name:
                     param.requires_grad = req_grad
-                #if "layer4" not in name:
-                    #param.requires_grad = req_grad
-                    
-        # Congela tutti i layer tranne layer3 e layer4
+        
         set_parameter_requires_grad(model, req_grad=False)
+        '''
 
-        num_ftrs = model.fc.in_features
-        model.fc = nn.Linear(num_ftrs, num_classes)
-    
-    elif model_name == "resnet34":
-        model = torchvision.models.resnet34(weights='IMAGENET1K_V1')
+
+        # Sblocca tutti i parametri per l'addestramento -> NOcong
+        for param in model.parameters():
+            param.requires_grad = True
+        
+
         num_ftrs = model.fc.in_features
         model.fc = nn.Linear(num_ftrs, num_classes)
 
