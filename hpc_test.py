a4db7f0f537fc42803bcdd8f60ba18a7110d8313
Arianna Cella <117760885+ariannaCella@users.noreply.github.com>
2025-05-05 17:31:31 +0200

    Update hpc_test.py

--- hpc_test.py
+++ hpc_test.py
@@ -4,10 +4,27 @@ import numpy as np
 from torch.utils.tensorboard import SummaryWriter
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
+import time
 
 from solver import Solver
-from dataset.dataset import load_dataset, load_dataset_6535 # load_dataset_70_30,
+from dataset.dataset import load_dataset, load_dataset_50_50
 from utils import plot_confusion_matrix, plot_confusion_matrix_with_errors, analyze_class_performance
+
+import random
+
+# setto un seed fisso (da scegliere) per rendere i risultati riproducibili
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+def seed_worker(worker_id,seed):
+        worker_seed = seed + worker_id
+        np.random.seed(worker_seed)
+        random.seed(worker_seed)
 
 
 def get_args():
@@ -20,6 +37,9 @@ def get_args():
     parser.add_argument('--batch_size', type=int, default=16, help='number of elements in batch size')
     parser.add_argument('--workers', type=int, default=2, help='number of workers in data loader')
     parser.add_argument('--print_every', type=int, default=500, help='print losses every N iteration')
+    parser.add_argument('--seed', type=int, default=42, help='random seed for reproducibility')
+    parser.add_argument('--aug', action='store_true', help='Enable data augmentation')
+
 
     parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
     parser.add_argument('--opt', type=str, default='SGD', choices=['SGD', 'Adam'], help = 'optimizer used for training')
@@ -36,15 +56,19 @@ def get_args():
 
 
 def main(args):
+    set_seed(args.seed)
+    # Generatore e worker seed
+    g = torch.Generator()
+    g.manual_seed(args.seed)
+    
     writer = SummaryWriter('./runs/' + args.run_name)
 
-    #trainset, valset, testset, class_names = load_dataset(args.dataset_path)
-    #trainset, valset, testset, class_names = load_dataset_70_30(args.dataset_path)
-    trainset, valset, testset, class_names = load_dataset_6535(args.dataset_path)
+    trainset, valset, testset, class_names = load_dataset(args.dataset_path,args.seed,args.aug)
+    #trainset, valset, testset, class_names = load_dataset_50_50(args.dataset_path,args.seed,args.aug)
     
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=args.workers)
-    validationloader = torch.utils.data.DataLoader(valset, batch_size=args.batch_size, shuffle=False, num_workers=args.workers)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, shuffle=False, num_workers=args.workers)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=args.workers, worker_init_fn=lambda worker_id: seed_worker(worker_id, args.seed), generator=g)
+    validationloader = torch.utils.data.DataLoader(valset, batch_size=args.batch_size, shuffle=False, num_workers=args.workers, worker_init_fn=lambda worker_id: seed_worker(worker_id, args.seed), generator=g)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, shuffle=False, num_workers=args.workers, worker_init_fn=lambda worker_id: seed_worker(worker_id, args.seed), generator=g)
 
     
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -60,7 +84,13 @@ def main(args):
             args=args)
 
     # TRAIN model
+    start_time = time.time()
     solver.train()
+    # Tempo di addestramento
+    elapsed = int(time.time() - start_time)
+    h, m = divmod(elapsed // 60, 60)
+    s = elapsed % 60
+    training_time_str = f"{h}h {m}m {s}s"
 
     # Test Model
     all_preds, all_labels = solver.test()
@@ -68,9 +98,29 @@ def main(args):
     # Confusion matrix
     cm = confusion_matrix(all_labels, all_preds)
     
+    # Analisi dei risultati
     plot_confusion_matrix(cm, class_names, writer=writer, global_step=args.epochs)
     plot_confusion_matrix_with_errors(cm, class_names, writer=writer, global_step=args.epochs)
-    analyze_class_performance(all_labels, all_preds, cm, class_names, top_n=5)
+    analyze_class_performance(all_labels, all_preds, cm, class_names, writer, top_n=5)
+    
+
+    # Logga tabella Markdown per recap dell'esecuzione
+    markdown_table = f"""
+| Metric                    | Value                         |
+|---------------------------|-------------------------------|
+| Model Name                | {args.model_name}             |
+| Net Name                  | {solver.net_model_name}       |
+| Batch Size                | {args.batch_size}             |
+| Learning Rate             | {args.lr}                     |
+| Optimizer                 | {args.opt}                    |
+| Epochs                    | {args.epochs}                 |
+| Patience                  | {solver.patience}             |
+| Best Validation Accuracy  | {solver.best_accuracy:.2f}%   |
+| Final Test Accuracy       | {solver.test_accuracy:.2f}%   |
+| Training Time             | {training_time_str}           |
+"""
+    writer.add_text("Experiment Summary", markdown_table, global_step=args.epochs)
+    writer.close()
     
 
 
